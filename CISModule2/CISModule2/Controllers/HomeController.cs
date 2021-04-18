6c13b113e0c42d49f3973bf02113efcbfd7eb632
Robert Saenz <rsaenz7266@southwestuniversity.edu>
2021-04-17 19:39:41 -0600

    Module 6 i believe

--- CISModule2/CISModule2/Controllers/HomeController.cs
+++ CISModule2/CISModule2/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace CISModule2.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
